6e68381cf80a14f74dcdbcba8feff3302785cc32
Michael Wendland <accounts@michiwend.com>
2014-12-16 15:13:15 +0100

    impl pkg timeout, moar fadingz

--- test.go
+++ test.go
@@ -24,31 +24,55 @@ var (
 
 type LabLeucht struct {
 	io.ReadWriteCloser
+	responseTimeout time.Duration
 }
 
 func (l *LabLeucht) sendPackage(pkg Package) error {
-	buf := make([]byte, 1)
 
 	fmt.Printf("Sending package [%3d %3d %3d %3d]\n", pkg[0], pkg[1], pkg[2], pkg[3])
+
+	var timeoutMult time.Duration
+
+	if pkg[0] < 251 {
+		timeoutMult = time.Duration(pkg[0]) * 10
+	}
+
+	timeout := time.After(time.Millisecond*timeoutMult + l.responseTimeout)
 
 	_, err := l.Write([]byte(pkg))
 	if err != nil {
 		return err
 	}
 
-	//TODO use go routine and timeout
-	for {
-		n, err := l.Read(buf)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if n != 0 {
-			break
-		}
-	}
+	rspCh := make(chan byte)
+	errCh := make(chan error)
 
-	if buf[0] != pkg.checkSum() {
-		return errors.New("bad response: got " + fmt.Sprint(buf[0]) + ", want " + fmt.Sprint(pkg.checkSum()))
+	go func() {
+		buf := make([]byte, 1)
+		for {
+			n, err := l.Read(buf)
+			if err != nil && err != io.EOF {
+				errCh <- err
+				return
+			}
+			if n != 0 {
+				rspCh <- buf[0]
+				return
+			}
+		}
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+
+	case r := <-rspCh:
+		if r != pkg.checkSum() {
+			return errors.New("bad response: got " + fmt.Sprint(r) + ", want " + fmt.Sprint(pkg.checkSum()))
+		}
+
+	case <-timeout:
+		return errors.New("response timed out")
 	}
 
 	return nil
@@ -69,6 +93,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	leucht.responseTimeout = 1 * time.Millisecond
 
 	time.Sleep(1 * time.Second)
 
@@ -99,18 +124,23 @@ func main() {
 
 	for {
 
-		var i byte
-
-		for i = 0; i < 254; i++ {
+		for i := 0; i < 255; i++ {
 			//time.Sleep(1 * time.Microsecond)
-			if err := leucht.sendPackage([]byte{254, i, byte(255 - i), 0}); err != nil {
+			if err := leucht.sendPackage([]byte{254, byte(255 - i), byte(i), 0}); err != nil {
 				fmt.Println(err)
 			}
 		}
 
-		for i = 255; i > 1; i-- {
+		for i := 0; i < 255; i++ {
 			//time.Sleep(1 * time.Microsecond)
-			if err := leucht.sendPackage([]byte{254, i, byte(255 - i), 0}); err != nil {
+			if err := leucht.sendPackage([]byte{254, 0, byte(255 - i), byte(i)}); err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		for i := 0; i < 255; i++ {
+			//time.Sleep(1 * time.Microsecond)
+			if err := leucht.sendPackage([]byte{254, byte(i), 0, byte(255 - i)}); err != nil {
 				fmt.Println(err)
 			}
 		}
